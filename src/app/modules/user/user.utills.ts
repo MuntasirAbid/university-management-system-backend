6bf969eb9c9e699fdb9716b7586306bfeb7b4f84
MuntasirAbid <muntasira7@hotmail.com>
2025-03-02 23:10:13 +0100

    fixed bugs

--- src/app/modules/user/user.utills.ts
+++ src/app/modules/user/user.utills.ts
@@ -1,7 +1,7 @@
 import { TAcademicSemester } from "../academicSemester/academicSemester.interface";
 import { User } from "./user.model";
 
-const findStudentId = async () => {
+const findLastStudentId = async () => {
   const lastStudent = await User.findOne(
     {
       role: "student",
@@ -16,12 +16,26 @@ const findStudentId = async () => {
     })
     .lean();
 
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+  return lastStudent?.id ? lastStudent.id : undefined;
 };
 
 export const generateStudentId = async (payload: TAcademicSemester) => {
   //first time
-  const currentId = (await findStudentId()) || (0).toString();
+  let currentId = (0).toString();
+
+  const lastStudentId = await findLastStudentId();
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6);
+  const lastStudentYear = lastStudentId?.substring(0, 4);
+  const currentSemesterCode = payload.code;
+  const currentYear = payload.year;
+
+  if (
+    lastStudentId &&
+    lastStudentSemesterCode === currentSemesterCode &&
+    lastStudentYear == currentYear
+  ) {
+    currentId = lastStudentId.substring(6);
+  }
 
   let incrementId = (Number(currentId) + 1).toString().padStart(4, "0");
 
