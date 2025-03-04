7d3290e315749b9302fde36d6622b893fd14aea3
MuntasirAbid <muntasira7@hotmail.com>
2025-03-04 03:43:29 +0100

    handle department validation

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -14,6 +14,30 @@ const academicDepartmentSchema = new Schema<TAcademicDepartment>(
   }
 );
 
+//middleware to check name whether before exist or not
+academicDepartmentSchema.pre("save", async function (next) {
+  const isDepartmentExist = await AcademicDepartment.findOne({
+    name: this.name,
+  });
+
+  if (isDepartmentExist) {
+    throw new Error("THis department already exist!");
+  }
+  next();
+});
+
+//middleware to check before update
+academicDepartmentSchema.pre("findOneAndUpdate", async function (next) {
+  const query = this.getQuery();
+
+  const isDepartmentExist = await AcademicDepartment.findOne(query);
+
+  if (!isDepartmentExist) {
+    throw new Error("Department is not exist!");
+  }
+  next();
+});
+
 export const AcademicDepartment = model<TAcademicDepartment>(
   "AcademicDepartment",
   academicDepartmentSchema
