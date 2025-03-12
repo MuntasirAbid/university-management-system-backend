d99086ef5069edc6f42de4dbe8164dd0b98af811
MuntasirAbid <muntasira7@hotmail.com>
2025-03-13 00:17:27 +0100

    model, interface, validation schema done

--- src/app/modules/semesterRegistration/semesterRegistrationValidation.ts
+++ src/app/modules/semesterRegistration/semesterRegistrationValidation.ts
@@ -1,7 +1,15 @@
 import { z } from "zod";
+import { SemesterRegistrationStatus } from "./semesterRegistration.const";
 
 const createSemesterRegistrationSchema = z.object({
-  body: z.object({}),
+  body: z.object({
+    academicSemester: z.string(),
+    status: z.enum([...(SemesterRegistrationStatus as [string, ...string[]])]),
+    startDate: z.string().datetime(),
+    endDate: z.string().datetime(),
+    minCredit: z.number(),
+    maxCredit: z.number(),
+  }),
 });
 
 export const SemesterRegistrationValidations = {
