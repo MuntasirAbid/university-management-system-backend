f724e7b10785023906d08b70bc6a8fa49eb4d153
MuntasirAbid <muntasira7@hotmail.com>
2025-03-19 02:36:01 +0100

    implemented authorization using auth

--- src/app/middleware/auth.ts
+++ src/app/middleware/auth.ts
@@ -2,15 +2,38 @@ import { NextFunction, Request, Response } from "express";
 import catchAsync from "../utils/catchAsync";
 import AppError from "../errors/AppError";
 import status from "http-status";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import config from "../config";
+import { TUserRole } from "../modules/user/user.interface";
 
-const auth = () => {
+const auth = (...requiredRoles: TUserRole[]) => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization;
 
+    //check if the token send from client
     if (!token) {
       throw new AppError(status.UNAUTHORIZED, "Unauthorized access");
     }
-    next();
+
+    //check if the is valid
+    jwt.verify(
+      token,
+      config.jwt_access_secret as string,
+      function (err, decoded) {
+        if (err) {
+          throw new AppError(status.UNAUTHORIZED, "Unauthorized access");
+        }
+
+        const role = (decoded as JwtPayload).role;
+
+        if (requiredRoles && !requiredRoles.includes(role)) {
+          throw new AppError(status.UNAUTHORIZED, "You are not authorized");
+        }
+
+        req.user = decoded as JwtPayload;
+        next();
+      }
+    );
   });
 };
 
